1bb643c70a348aa1c9c8409ace5b3851d898c355
Liban Tahsin <tahsin.liban0927@gmail.com>
2025-01-30 22:09:13 +0600

    first commit

---
+++ Break.py
@@ -0,0 +1,10 @@
+a = input("Enter a Word: ")
+
+for i in a:
+    if i =="A" or i == "a":
+        
+        print("A is Found")
+        break
+    
+    else:
+        print(f"A not Found For [{i}]")
---
+++ Continue.py
@@ -0,0 +1,12 @@
+var = 10
+
+while var > 0:
+    
+    var = var - 1
+    
+    if var ==5 or var == 8:
+        continue
+    
+    print("\nCurrent Variable valur :", var)
+    
+print("\nGood Bye!")
